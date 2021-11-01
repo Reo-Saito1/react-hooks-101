58eda0783bf7f520c41f5f27a46be32e42f490ef
齋藤礼欧 <saitoreo@saitouayaounoMacBook-Pro.local>
2021-11-01 18:52:07 +0900

    my first React With hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const App = () => {
-  return (
+  const [count,setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  const decrement = () => {
+    setCount(count - 1)
+  }
+
+  const reset = () => {
+    setCount(0)
+  }
+
+  const dowble = () => {
+    setCount(count * 2)
+  }
+
+  const divide3 = () => {
+    setCount(previousCount => {
+      return previousCount %3 === 0 ? previousCount/3:previousCount
+    })
+  }
+
+
+   return (
+     <>
     <div>
-      This is a template for React app.
+      count:{count}
     </div>
+    <div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    </div>
+    <div>
+    <button onClick={reset}>reset</button>
+    <button onClick={dowble}>dowble</button>
+    <button onClick={divide3}>3の倍数で割ったものを判別</button>
+    </div>
+    </>
   );
 }
 
