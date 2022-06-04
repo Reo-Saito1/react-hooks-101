393d70662f3512ab2606a79b0f47f96e4cb3a604
齋藤礼欧 <saitoreo@saitouayaounoMacBook-Pro.local>
2022-06-04 15:31:38 +0900

    diplay events refactor and implement remove event button

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,14 @@
-const events = (state = [],action) => {
-    switch(action.type){
+const events = (state = [], action) => {
+    switch (action.type) {
         case 'CREATE_EVENT':
-            const event = {title:action.title,body:action.body}
+            const event = { title: action.title, body: action.body }
             const length = state.length
-            let id = length === 0 ? 1 : state[length-1].id + 1
-            return [...state, {id, ...event}]
+            let id = length === 0 ? 1 : state[length - 1].id + 1
+            return [...state, { id, ...event }]
         case 'DELETE_EVENT':
-            return state
+            return state.filter(event => event.id !== action.id);
         case 'DELETE_ALL_EVENT':
-          return []
+            return []
         default:
             return state
     }
