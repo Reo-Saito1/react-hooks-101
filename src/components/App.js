3245e78dd69ed30492364c3129683b10e358d1a8
齋藤礼欧 <saitoreo@saitouayaounoMacBook-Pro.local>
2022-06-04 16:34:06 +0900

    refactor components

--- src/components/App.js
+++ src/components/App.js
@@ -1,70 +1,15 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import reducer from '../reducers'
-import Event from '../components/Event';
+import Events from '../components/Events';
+import EventForm from '../components/EventForm';
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  const addEvent = e => {
-    //画面が真っ白になるイベントが消える
-    e.preventDefault();
-
-    //dispacth
-    dispatch({
-      type: 'CREATE_EVENT',
-      title,
-      body
-    })
-
-    setTitle('');
-    setBody('');
-  }
-
-  console.log({ state })
-
-
-  const deleteAllEvents = e => {
-    e.preventDefault();
-
-    const result = window.confirm('すべてのイベントを本当に削除しても良いですか？')
-
-    if (result) dispatch({ type: 'DELETE_ALL_EVENTS' })
-  }
-
-  const unCreate = title === '' || body === ''
   return (
     <div className="container-fluid">
-      <h4>イベント作成フォーム</h4>
-      <form>
-        <div className="form-group">
-          <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle" value={title} onChange={e => { setTitle(e.target.value) }} />
-          <div className="form-group">
-            <label htmlFor="formEventBody">ボディ</label>
-            <textarea className="form-control" id="formEventBody" value={body} onChange={e => { setBody(e.target.value) }} />
-          </div>
-          <button className="btn btn-primary" onClick={addEvent} disabled={unCreate}>イベントを作成する</button>
-          <button className="btn btn-danger " onClick={deleteAllEvents} disabled={state.length === 0}>イベントを削除する</button>
-          <h4>イベント一覧</h4>
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>タイトル</th>
-                <th>ボディ</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
-            </tbody>
-          </table>
-
-        </div>
-      </form>
+      <EventForm state={state} dispatch={dispatch} />
+      <Events state={state} dispatch={dispatch} />
     </div>
   );
 }
